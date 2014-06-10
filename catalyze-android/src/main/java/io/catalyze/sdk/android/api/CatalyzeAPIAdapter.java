6bb2d9d456cc6bd61bb78309bd8d217c02d3c18b
Josh Ault <josh.ault3@gmail.com>
2014-06-10 10:42:07 -0500

    replaced my local API url with production

--- catalyze-android/src/main/java/io/catalyze/sdk/android/api/CatalyzeAPIAdapter.java
+++ catalyze-android/src/main/java/io/catalyze/sdk/android/api/CatalyzeAPIAdapter.java
@@ -1,46 +1,20 @@
 package io.catalyze.sdk.android.api;
 
-import android.net.SSLCertificateSocketFactory;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.squareup.okhttp.OkHttpClient;
-
-import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
-
-import java.lang.reflect.Modifier;
-import java.security.KeyStore;
-
-import javax.net.ssl.SSLSocketFactory;
 
 import retrofit.RestAdapter;
 import retrofit.client.OkClient;
-import retrofit.converter.GsonConverter;
 
 public class CatalyzeAPIAdapter {
 
     private static CatalyzeAPI api = new RestAdapter.Builder()
             .setLogLevel(RestAdapter.LogLevel.FULL)
-            .setEndpoint("https://10.23.16.103:8443/v2")
+            .setEndpoint("https://apiv2.catalyze.io/v2")
             .setRequestInterceptor(new AuthorizationInterceptor())
-            .setClient(new OkClient(configureClient()))
+            .setClient(new OkClient(new OkHttpClient()))
             .build().create(CatalyzeAPI.class);
 
     private CatalyzeAPIAdapter() { }
-
-    private static OkHttpClient configureClient() {
-        OkHttpClient client = new OkHttpClient();
-        client.setHostnameVerifier(new AllowAllHostnameVerifier());
-        try {
-            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            trustStore.load(null, null);
-            SSLSocketFactory sf = SSLCertificateSocketFactory.getInsecure(60000, null);
-
-            client.setSslSocketFactory(sf);
-        } catch (Exception e) {
-        }
-        return client;
-    }
 
     public static CatalyzeAPI getApi() {
         return api;
